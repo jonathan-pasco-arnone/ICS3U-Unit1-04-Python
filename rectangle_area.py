2bc45851d6f57cd828c0f35d8dd3088daec2c13d
Ubuntu <ubuntu@ip-172-31-33-231.ec2.internal>
2020-11-18 22:59:40 +0000

    initial commit

---
+++ rectangle_area.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by Jonathan Pasco-Arnone
+# Created on November 2020
+# This program does calculates the area and perimiter of a rectangle
+
+def main():
+    print("If a rectangle has the dimensions")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(5*3))
+    print("Perimeter is {}cm".format(5+5+3+3))
+
+
+if __name__ == "__main__":
+    main()
